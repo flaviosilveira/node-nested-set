df47217df710f424d60e13165b89ad371f3c39a7
Flavio <flavioaugustosilveira@gmail.com>
2021-08-28 17:02:19 -0300

    New NestedSetProperties

--- lib/nested-set-properties.ts
+++ lib/nested-set-properties.ts
@@ -1,6 +1,16 @@
 export interface NestedSetProperties {
-  depth: number;
-  left: number;
-  right: number;
+  uuid: string;
+  org_uuid: string;
   title: string;
+  tag: string;
+  node_left: number;
+  node_right: number;
+  node_depth: number;
+  type: string;
+  created_by: string;
+  updated_by: string;
+  created_at: string;
+  updated_at: string;
+  deleted_by: string;
+  deleted_at: string;
 }
